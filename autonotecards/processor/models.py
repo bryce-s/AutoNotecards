3ac8795c98207cb506c5eaaef061c209a8a60335
bryce-s <bryces@umich.edu>
2018-04-20 20:34:42 -0400

    recursive formatting, working on models

--- autonotecards/processor/models.py
+++ autonotecards/processor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class PdfPack(models.Model):
+    """django model--represents a database"""
+    desc = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents/')
+    uploadTime = models.DateTimeField(auto_now_add=True)
+    # note; this will upload to $media_root/documents/YYYY/MM/DD
--- bin/formatCode.sh
+++ bin/formatCode.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-set -e
 set -x
 # npm -g install js-beautify (for js and html..)
-js-beautify -r *.html
+# recurse through dir applying formatters
+for f in autonotecards/*;
+  do 
+     [ -d $f ] && cd "$f" && autopep8 --in-place --aggressive --aggressive *.py && js-beautify -r *.html
+     cd ..
+  done; 
