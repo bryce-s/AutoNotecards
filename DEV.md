b22c9b34362f13b141e75032f3d55e9e5194e4f8
bryce-s <bryces@umich.edu>
2018-04-21 11:15:39 -0400

    ready to migrate

--- DEV.md
+++ DEV.md
@@ -4,4 +4,9 @@ conda create -n NoteCards python=3.6 anaconda # create virtual env
 source activate NoteCards
 pip install Django
 pip install pylint_django
+in vscode settings;
+   "python.linting.pylintArgs": [
+        "--load-plugins=pylint_django"
+    ],
 ```
+
--- autonotecards/processor/models.py
+++ autonotecards/processor/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class PdfPack(models.Model):
-    """django model--represents a database"""
+    """django model--represents a database. PdfPack.ID is auto init'd"""
     desc = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
     uploadTime = models.DateTimeField(auto_now_add=True)
-    # note; this will upload to $media_root/documents/YYYY/MM/DD
+    created_by = models.ForeignKey(User, related_name='PDFPack')
+    # note; upload time will upload to $media_root/documents/YYYY/MM/DD
