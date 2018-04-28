82081f30192f52c80c6241142e5024ca01a26c25
bryce-s <bryces@umich.edu>
2018-04-28 00:21:41 -0400

    ready to carve

--- imagecarver/src/NotecardCarver.cpp
+++ imagecarver/src/NotecardCarver.cpp
@@ -1,8 +1,28 @@
 #include "NotecardCarver.h"
 
+void NotecardCarver::carveImage(PageVertex &vertex, std::vector<unsigned char> &image) {
+
+    if (vertex.pageNumber == prev.pageNumber) {
+        // then we we carve from there..
+        // also, vertex.pageNumber >= 0
+        assert(vertex.pageNumber >= 0);
+
+    }
+    prev = vertex;
+}
 
 void NotecardCarver::processImages(std::string inputDir) {
     for (size_t i{0}; i < targets.size(); i++) {
-
+        std::vector<unsigned char> image;
+        unsigned width, height;
+        // decode
+        unsigned error = lodepng::decode(image, width, height, "tests/1.png")
+        if (error) {
+            std::cout << "decode error " << error << ": " << lodepng_error_text(error) << "\n";
+        } else {
+            carveImage(targets[i], image);
+        }
     }
 }
+
+
--- imagecarver/src/NotecardCarver.h
+++ imagecarver/src/NotecardCarver.h
@@ -1,17 +1,21 @@
 #pragma once
 
 #include "ImageTargets.h"
+#include "lodepng.h"
 #include <string>
-
+#include <vector>
 class NotecardCarver {
     ImageTargets targets;
+    PageVertex prev;
 
 public:
-    NotecardCarver(const ImageTargets targets_in) : targets(targets_in) {
+    NotecardCarver(const ImageTargets targets_in) : targets(targets_in), prev(-1,-1,-1) {
 
     }
+
+    void carveImage(PageVertex& vertex, std::vector<unsigned char>& image);
 
     void processImages(std::string inputDir);
 
 };
-
+s
